e592dc2cc41c0eeca633a109601e2147cae8c7ad
Sacha <sachacollev@gmail.com>
2021-03-23 21:35:07 +0100

    non riesco a far aprire un solo ul con i li

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-function clickActive() {
-    $('.fa-minus-square,.fa-bars').toggleClass('js-active');
-    console.log('click');
-  };
+function activeMenu() {
+  var onOffMenu = $('.fa-minus-square,.fa-bars,.ul_container');
+  onOffMenu.toggleClass('js-active');
+  console.log('click');
+};
+function activeList() {
+  $('.li_active').click(function () {
+    var clickLi = $(this);
+    var clickedLi = clickLi.find('?');
+    //non riesco a far aprire un solo ul_elem con i li
+    //clickedLi.toggleClass('js-active');
+    //$('.li_elem').toggleClass('js-active');
+  })
 
+}
 function init() {
- $('.fa-bars,.fa-minus-square').click(clickActive);
+ $('.fa-bars,.fa-minus-square').click(activeMenu);
+ activeList();
 }
 init();
 $(document).ready(init);
